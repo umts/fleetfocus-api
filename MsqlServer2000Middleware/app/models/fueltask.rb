4ea16b1b7448c5349f270ada3417a3cd4b208625
Jonathan Katon <jkaton@student.umass.edu>
2012-01-03 15:32:58 -0500

    deleted a bunch o stuff

--- MsqlServer2000Middleware/app/models/fueltask.rb
+++
@@ -1,3 +0,0 @@
-class Fueltask < ActiveRecord::Base
-  establish_connection :fueltask
-end
--- MsqlServer2000Middleware/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '89014d352440e8741c2d16a99876fea07613f7dd324994e68d6be270312e26710c19c38d912bd8b8148f30e15b04ad61b8a95021632f2baca4c08e4bfabfa1c2';
