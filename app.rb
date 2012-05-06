5f13d264a1acacae3f83aa87ef5f94bf82e943cc
Matt Moretti <werebus@gmail.com>
2012-05-05 22:18:57 -0400

    Modify app to read database config from database.yml
    Note that the config file is a little different than your average rails
    app.  First, there's no environments because, well, we're always
    connecting (read-only) to the live db.  Second, the fields all have
    colons in the file because it was easier than requiring active_support
    or writing a method to symbolize the keys.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'active_record'
 require 'activerecord-sqlserver-adapter'
 require 'tiny_tds'
 require 'haml'
+require 'yaml'
 require 'uri'
 require 'json'
 require 'date'
@@ -12,13 +13,9 @@ require 'date'
 # This section establishes the Msql Server 2000 connection #
 ############################################################
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlserver',
-  :host => 'FILL_THIS_IN',
-  :database => 'FILL_THIS_IN',
-  :username => 'FILL_THIS_IN',
-  :password => 'FILL_THIS_IN'
-)
+# Is this the right scope?
+@@database_config = YAML.load_file( File.join( File.dirname( __FILE__ ), "config", "database.yml" ) )
+ActiveRecord::Base.establish_connection( @@database_config )
 
 ##########################################################################################
 # This section takes the parameters from url calls and obtains data from the msql server #
@@ -28,46 +25,44 @@ class SqlServer2000Connection < ActiveRecord::Base
   set_table_name 'emsdba.FTK_MAIN'
   self.primary_key = 'row_id'
   @@have_tried_to_reconnect = false
-  
-  #overriding rails find method to work with fueltask
+
+  #overriding rails find method to work with the fa db
   def self.find(*args)
     json_return = {:connection_valid => false, :error => "" ,:fueling => nil}
-    
+
     begin
+
       options = args.extract_options!
       json_return[:fueling] = with_scope :find => options do
-        super(args.first,
-        :select => "qty_fuel as amount, meter_1 as mileage, ftk_date as time_at, row_id as fuel_focus_row_id, X_datetime_insert as time_at_insertion, EQ_equip_no"
+        super(args.first, :select =>
+              "qty_fuel as amount, meter_1 as mileage, ftk_date as time_at,
+               row_id as fuel_focus_row_id, X_datetime_insert as time_at_insertion,
+               EQ_equip_no"
         )
       end
-      
+
       json_return[:connection_valid] = true
-      
+
       if json_return[:fueling] == []
         @@have_tried_to_reconnect = false
         json_return[:error] = "Your query has returned no results. Please contact MIS for further assitance. (vehicle by id)"
-      end    
+      end
+
     rescue
+
       if @@have_tried_to_reconnect
         @@have_tried_to_reconnect = false
-        json_return[:error] = "You  are currently not connected to the database. Please contact MIS for further assitance. (vehicle by id)"
+        json_return[:error] = "You are currently not connected to the database. Please contact MIS for further assitance. (vehicle by id)"
       else
-        ActiveRecord::Base.establish_connection(
-          :adapter => 'sqlserver',
-          :host => 'FILL_THIS_IN',
-          :database => 'FILL_THIS_IN',
-          :username => 'FILL_THIS_IN',
-          :password => 'FILL_THIS_IN'
-          )
+        ActiveRecord::Base.establish_connection( @@database_config )
         @@have_tried_to_reconnect = true
         retry
       end
+
     end
-    
+
     json_return
-    
   end
-      
 end
 
 
@@ -76,12 +71,16 @@ end
 ################################################################################################################
 
 before do
-  
+
 end
 
 # Obtain all records for a vehicle by its id
 get '/vehicle/:name' do
-  @vehicle_by_id = SqlServer2000Connection.find(:all, :conditions => ["EQ_equip_no = ?", params[:name]], :order => "ftk_date desc")
+  @vehicle_by_id =
+    SqlServer2000Connection.find(:all,
+                                 :conditions => ["EQ_equip_no = ?", params[:name]],
+                                 :order => "ftk_date desc")
+
   content_type :json
   @vehicle_by_id.to_json
   #haml :vehicle_id
@@ -89,7 +88,11 @@ end
 
 # Obtain all records for a vehicle by its id which occur after the provided datetime
 get '/vehicle/:name/:datetime' do
-  @vehicle_by_id_datetime = SqlServer2000Connection.find(:all, :conditions => ["EQ_equip_no = ? AND ftk_date > ?", params[:name], Time.at(params[:datetime].to_f)], :order => "ftk_date desc")
+  @vehicle_by_id_datetime =
+    SqlServer2000Connection.find(:all,
+                                 :conditions => ["EQ_equip_no = ? AND ftk_date > ?", params[:name], Time.at(params[:datetime].to_f)],
+                                 :order => "ftk_date desc")
+
   content_type :json
   @vehicle_by_id_datetime.to_json
   #haml :vehicle_id_datetime
@@ -97,7 +100,11 @@ end
 
 # Obtain all records for a vehicle by its id which occur between the provided datetimes
 get '/vehicle/:name/:start_datetime/:end_datetime' do
-  @vehicle_by_id_daterange = SqlServer2000Connection.find(:all, :conditions => ["EQ_equip_no = ? AND ftk_date > ? AND ftk_date < ?", params[:name], Time.at(params[:start_datetime].to_f), Time.at(params[:end_datetime].to_f)], :order => "ftk_date desc")
+  @vehicle_by_id_daterange =
+    SqlServer2000Connection.find(:all,
+                                 :conditions => ["EQ_equip_no = ? AND ftk_date > ? AND ftk_date < ?", params[:name], Time.at(params[:start_datetime].to_f), Time.at(params[:end_datetime].to_f)],
+                                 :order => "ftk_date desc")
+
   content_type :json
   @vehicle_by_id_daterange.to_json
   #haml :vehicle_id_daterange
@@ -105,7 +112,11 @@ end
 
 # Obtain records for all vehicles which occur after the provided datetime
 get '/all/:datetime' do
-  @all_vehicles_by_datetime = SqlServer2000Connection.find(:all, :conditions => ["ftk_date > ?", Time.at(params[:datetime].to_f)], :order => "ftk_date desc")
+  @all_vehicles_by_datetime =
+    SqlServer2000Connection.find(:all,
+                                 :conditions => ["ftk_date > ?", Time.at(params[:datetime].to_f)],
+                                 :order => "ftk_date desc")
+
   content_type :json
   @all_vehicles_by_datetime.to_json
   #haml :all_vehicles_datetime
@@ -113,7 +124,11 @@ end
 
 # Obtain records for all vehicles which occur between two dates
 get '/all/:start_datetime/:end_datetime' do
-  @all_vehicles_by_daterange = SqlServer2000Connection.find(:all, :conditions => ["ftk_date > ? AND ftk_date < ?", Time.at(params[:start_datetime].to_f), Time.at(params[:end_datetime].to_f)], :order => "ftk_date desc")
+  @all_vehicles_by_daterange =
+    SqlServer2000Connection.find(:all,
+                                 :conditions => ["ftk_date > ? AND ftk_date < ?", Time.at(params[:start_datetime].to_f), Time.at(params[:end_datetime].to_f)],
+                                 :order => "ftk_date desc")
+
   content_type :json
   @all_vehicles_by_daterange.to_json
   #haml :all_vehicles_daterange
@@ -152,19 +167,19 @@ __END__
   %head
   %body
     =@vehicle_by_id_datetime.to_json
-    
+
 @@vehicle_id_daterange
 %html
   %head
   %body
     =@vehicle_by_id_daterange.to_json
-    
+
 @@all_vehicles_datetime
 %html
   %head
   %body
     =@all_vehicles_by_datetime.to_json
-    
+
 @@all_vehicles_daterange
 %html
   %head
