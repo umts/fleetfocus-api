dce54ca28c9033785cb48eea8528ec7f6c82f61e
Matt Moretti <werebus@gmail.com>
2016-12-08 13:12:58 -0500

    Working factory

--- spec/factories/fuelings.rb
+++ spec/factories/fuelings.rb
@@ -2,11 +2,11 @@ FactoryGirl.define do
   sequence :row_id
 
   factory :fueling do
-    qty_fuel
-    meter_1
-    ftk_date
+    qty_fuel 0.0
+    meter_1 0
+    ftk_date 1.day.ago
     row_id
-    add_attribute(:X_datetime_insert)
-    add_attribute(:EQ_equip_no)
+    add_attribute(:X_datetime_insert, 1.day.ago)
+    add_attribute(:EQ_equip_no, '3201')
   end
 end
