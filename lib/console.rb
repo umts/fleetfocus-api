3b616e0b54f1034396b24adb0bb1554362733948
Matt Moretti <werebus@gmail.com>
2016-12-09 13:55:23 -0500

    Don't need to manually establish connection anymore

--- lib/console.rb
+++ lib/console.rb
@@ -3,14 +3,8 @@ require 'yaml'
 require 'json'
 require 'bundler'
 
-this_dir = File.dirname(__FILE__)
-
 env = ENV['RACK_ENV'] || 'development'
 Bundler.require(:default, env)
-
-config_file = File.join(this_dir, '..', 'config', 'database.yml')
-db_config = YAML.load_file(config_file)[env]
-ActiveRecord::Base.establish_connection(db_config)
 
 require 'fueling'
 require 'eam_app'
