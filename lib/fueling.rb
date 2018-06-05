0fd5487a9d3b608eed3d8761b16b219f5da20270
Matt Moretti <werebus@gmail.com>
2018-06-05 17:31:15 -0400

    Times in the FA database are local. Closes #23
    Have they always been? I don't think so, but this is the "fix", I guess.
    This fix also required more wrangling in the specs to get around the
    SQLite bug with `AS` clauses. Yuck.
    
    Also added a meaningful `inspect` method for the console.

--- lib/fueling.rb
+++ lib/fueling.rb
@@ -2,6 +2,7 @@
 class Fueling < ActiveRecord::Base
   self.table_name = 'emsdba.FTK_MAIN'
   self.primary_key = 'row_id'
+  self.default_timezone = :local
 
   default_scope do
     order('ftk_date DESC').select 'qty_fuel AS amount',
@@ -10,5 +11,12 @@ class Fueling < ActiveRecord::Base
                                   'row_id AS fuel_focus_row_id',
                                   'X_datetime_insert AS time_at_insertion',
                                   'EQ_equip_no'
+  end
+
+  def inspect
+    attrs = { vehicle: self.EQ_equip_no,
+              amount: amount.to_f,
+              time_at: time_at }
+    "#<#{self.class} #{attrs}>"
   end
 end
