8d02ed9797f086be592390a203b99bc4044a2e7d
Matt Moretti <werebus@gmail.com>
2018-11-20 16:20:34 -0500

    Use FactoryBot non-static attributes

--- spec/factories/fuelings.rb
+++ spec/factories/fuelings.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
   sequence :row_id
 
   factory :fueling do
-    qty_fuel 0.0
-    meter_1 0
-    ftk_date 1.day.ago
+    qty_fuel { 0.0 }
+    meter_1 { 0 }
+    ftk_date { 1.day.ago }
     row_id
-    add_attribute(:X_datetime_insert, 1.day.ago)
-    add_attribute(:EQ_equip_no, '3201')
+    add_attribute(:X_datetime_insert) { 1.day.ago }
+    add_attribute(:EQ_equip_no) { '3201' }
   end
 end
