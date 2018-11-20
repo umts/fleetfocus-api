169392da34ba0e8556b7371099ff2240e379ed80
Matt Moretti <werebus@gmail.com>
2018-11-20 16:22:09 -0500

    Added "focus" setting.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 ENV['RACK_ENV'] = 'test'
 
 require 'bundler'
@@ -16,7 +17,13 @@ end
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.warnings = true
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   config.order = :random
+  Kernel.srand config.seed
+
   config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.include RSpecMixin
