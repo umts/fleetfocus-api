e6d73b77fdd89766de9edf0f6b647c1ff42e34bf
Matt Moretti <werebus@gmail.com>
2019-05-28 12:26:35 -0400

    Make a set of migrations that works in 5 and 4

--- db/migrate/20190528161310_create_fuelings_again.rb
+++ db/migrate/20190528161310_create_fuelings_again.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
-class CreateFuelings < ActiveRecord::Migration
-  def change
-    return if ActiveRecord.version >= Gem::Version.new('5.0')
+class CreateFuelingsAgain < ActiveRecord::Migration
+  def up
+    return if table_exists? 'FTK_MAIN'
 
     # note that this is a partial table compared to what's in the EAM DB,
     # but it contains all of the columns we actually use in this application.
-    create_table 'emsdba.FTK_MAIN', primary_key: 'row_id' do |t|
+    create_table 'FTK_MAIN', primary_key: 'row_id' do |t|
       t.decimal  'qty_fuel'
       t.integer  'meter_1'
       t.datetime 'ftk_date'
       t.datetime 'X_datetime_insert'
       t.string   'EQ_equip_no'
     end
+  end
+
+  def down
+    return if ActiveRecord.version < Gem::Version.new('5.0')
+
+    drop_table 'FTK_MAIN'
   end
 end
