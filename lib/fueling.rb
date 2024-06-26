647e2a75c02893668bdfd5caeae687af8a2b7bc4
benmelz <ben@melz.me>
2024-06-26 16:37:19 -0400

    set super strict minimum coverage

--- lib/fueling.rb
+++ lib/fueling.rb
@@ -3,11 +3,9 @@
 ActiveRecord.default_timezone = :local
 
 class Fueling < ActiveRecord::Base
+  # :nocov:
   self.table_name_prefix = 'emsdba.' unless ENV['RACK_ENV'] == 'test'
+  # :nocov:
   self.table_name = 'FTK_MAIN'
   self.primary_key = 'row_id'
-
-  def readonly?
-    ENV['RACK_ENV'] == 'test' ? super : true
-  end
 end
