0408fabef740fc7240a28609d6ffd91384ec213c
benmelz <ben@melz.me>
2025-04-08 15:16:10 -0400

    automatically create log dir

--- lib/app_logs.rb
+++ lib/app_logs.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require 'fileutils'
 require 'pathname'
 
 module AppLogs
   class << self
     def registered(app)
       @env = app.settings.environment
+
       app.set :access_log, access_log!
       app.set :error_log, error_log!
 
@@ -15,16 +17,20 @@ module AppLogs
       app.before { env['rack.errors'] = app.settings.error_log }
     end
 
+    private
+
     def access_log!
+      FileUtils.mkdir_p log_dir
       log_path('access').open('a+').tap { |log| log.sync = true }
     end
 
     def error_log!
+      FileUtils.mkdir_p log_dir
       log_path('error').open('a+').tap { |log| log.sync = true }
     end
 
-    def log_path(type)
-      Pathname(__dir__).expand_path.join('../log', "#{@env}_#{type}.log")
-    end
+    def log_path(type) = log_dir.join("#{@env}_#{type}.log")
+
+    def log_dir = Pathname(__dir__).join('../log').expand_path
   end
 end
