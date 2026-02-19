691e7e1d3d19d277cde0e1a3fe8ccecdc57c0015
Matt Moretti <werebus@gmail.com>
2026-02-19 10:33:40 -0500

    Tell Capistrano we're using Bundler 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # config valid only for current version of Capistrano
-lock '~> 3.14'
+lock '~> 3.20'
 
 set :application, 'fleetfocus-api'
 set :repo_url, 'https://github.com/umts/fleetfocus-api.git'
@@ -15,5 +15,6 @@ append :linked_files, 'config/fleetfocus-api.key'
 append :linked_dirs, '.bundle', 'log', 'tmp/pids'
 
 set :passenger_restart_with_sudo, true
+set :bundle_version, 4
 
 before 'deploy:publishing', 'db:check'
